6095945795812a6feb43f70068e3702f570f791a
dilpreetsandhu1 <35703488+dilpreetsandhu1@users.noreply.github.com>
2023-02-22 01:19:10 -0700

    Database and API requests started
    Setup basic db to test API, requests currently not returning correct info/crashes on post, just need to work on routes file, will do Wednesday,

---
+++ backend/model/model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    Id: {
+        required: true,
+        type: Number
+    },
+    color: {
+        required: true,
+        type: String
+    },
+    row: {
+        required: true,
+        type: Number
+    },
+    column: {
+        required: true,
+        type: Number
+    },
+    dateTime: {
+        required: true,
+        type: String
+    }
+})
+
+module.exports = mongoose.model('Data', dataSchema)
+
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,29 @@
+// Hidden file, contains database URL
+require('dotenv').config();
+
+const routes = require('./routes/routes');
 const express = require('express')
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL
+
+
+// Connect to database
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
 const app = express()
 const port = 4000
 
+app.use('/api', routes)
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
