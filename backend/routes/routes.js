07ecbdfe8dcd2b6b65d188ad1cf6d3aa3c986dbb
dilpreetsandhu1 <35703488+dilpreetsandhu1@users.noreply.github.com>
2023-02-22 19:08:02 -0700

    Update routes.js

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -5,18 +5,6 @@ const router = express.Router()
 
 module.exports = router;
 
-//Get by ID Method
-router.get('/getOne/:id', async (req, res) => {
-    try{
-        const data = await Model.findById(req.params.id);
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
-
-
 //Get all data
 router.get('/getAll', async (req, res) => {
     try{
@@ -26,14 +14,4 @@ router.get('/getAll', async (req, res) => {
     catch(error){
         res.status(500).json({message: error.message})
     }
-})
-
-//Get data by ID
-router.get('/getOne/:id', (req, res) => {
-    res.send(req.params.id)
-})
-
-//Update data by ID
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API')
 })
