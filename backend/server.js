1bdf85437503ba72d79e0598f4054bbe8f11d58c
AbelTekeste <47335877+AbelTekeste@users.noreply.github.com>
2023-02-22 20:02:22 -0700

    Sockets added
    Added some socket stuff, gonna need to test with frontend to be sure it works.

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const express = require('express')
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL
 
-
 // Connect to database
 mongoose.connect(mongoString);
 const database = mongoose.connection;
@@ -22,12 +21,24 @@ database.once('connected', () => {
 const app = express()
 const port = 4000
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 app.use('/api', routes)
 app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const server = app.listen(port, () => {
-    console.log(`listening on *: ${port}`)
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
+});
+
+server.listen(port, () => {
+  console.log('listening on *:' + port);
+});
