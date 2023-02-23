12c9141444ef65941cdee6636c24da051e4089ab
ahmedamer2 <63519817+ahmedamer2@users.noreply.github.com>
2023-02-22 21:01:29 -0700

    Added mouse positioning and color picking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ const COLORS = [
 	"#820080",
 ];
 
-const CANVAS_WIDTH = 900;
+const CANVAS_WIDTH = 1300;
 const CANVAS_HEIGHT = 850;
 
 const ORIGIN = {
@@ -55,12 +55,14 @@ function scalePoint(p1, scale) {
 function App() {
 	const canvasRef = useRef(null);
 	const [context, setContext] = useState(null);
-	const [scale, setScale] = useState(ORIGIN);
+	const [scale, setScale] = useState(null);
 	const [offset, setOffset] = useState(ORIGIN);
 	const [mousePos, setMousePos] = useState(ORIGIN);
+	const [relMousePos, setRelMousePos] = useState(ORIGIN);
 	const [viewportTopLeft, setViewportTopLeft] = useState(ORIGIN);
 	const lastMousePosRef = useRef(ORIGIN);
 	const lastOffsetRef = useRef(ORIGIN);
+	const currentColor = useRef(null);
 
 	// update last offset
 	useEffect(() => {
@@ -226,6 +228,31 @@ function App() {
 		return () => canvasElem.removeEventListener("wheel", handleWheel);
 	}, [context, mousePos.x, mousePos.y, scale]);
 
+	// maintain the relative mouse position
+	useEffect(() => {
+		if (context) {
+			const storedTransform = context.getTransform();
+			const newRelative = context
+				.getTransform()
+				.invertSelf()
+				.transformPoint(mousePos);
+			context.setTransform(storedTransform);
+			relMousePos.x = Math.floor(newRelative.x / 10);
+			relMousePos.y = Math.floor(newRelative.y / 10);
+		}
+	}, [context, mousePos, relMousePos]);
+
+	function changeColor(color) {
+		currentColor.current = color;
+		let currentSelection = document.getElementsByClassName("selected");
+		if (currentSelection.length !== 0) {
+			currentSelection[0].textContent = "";
+			currentSelection[0].classList.remove("selected");
+		}
+		document.getElementById(color).classList.add("selected");
+		document.getElementById(color).textContent = "selected";
+	}
+
 	return (
 		<div id="main">
 			<canvas
@@ -240,11 +267,13 @@ function App() {
 					{COLORS.map((col) => {
 						return (
 							<div
+								id={col}
 								key={col}
 								className="color"
 								style={{
 									backgroundColor: col,
 								}}
+								onClick={() => changeColor(col)}
 							></div>
 						);
 					})}
