c40a730992c26d8423ef5c1fb92aed0d756de350
AbelTekeste <47335877+AbelTekeste@users.noreply.github.com>
2023-02-24 11:27:23 -0700

    Brought back the routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,33 @@
+const express = require('express');
+const Model = require('../model/model');
 const express = require("express");
 const Model = require("../model/model");
 
-const router = express.Router();
+const router = express.Router()
 
 module.exports = router;
+
+//Get by ID Method
+router.get('/getOne/:id', async (req, res) => {
+    try{
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+//Get data by ID
+router.get('/getOne/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+//Update data by ID
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+})
 
 //Get all data
 router.get("/getAll", async (req, res) => {
