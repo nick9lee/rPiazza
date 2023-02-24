7d2c38af98cd22c6c4870a29537fc4e59411dfad
ahmedamer2 <63519817+ahmedamer2@users.noreply.github.com>
2023-02-24 11:07:51 -0700

    made route return 2d array a

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const Model = require('../model/model');
+const express = require("express");
+const Model = require("../model/model");
 
-const router = express.Router()
+const router = express.Router();
 
 module.exports = router;
 
 //Get all data
-router.get('/getAll', async (req, res) => {
-    try{
-        const data = await Model.find();
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
-
+router.get("/getAll", async (req, res) => {
+	try {
+		const data = await Model.find();
+		let transformedArray = [...Array(200)].map((e) => Array(200));
+		console.log(transformedArray);
+		data.forEach((entry, index) => {
+			transformedArray[Math.floor(index / 200)][index % 200] = entry;
+		});
+		res.json(transformedArray);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
