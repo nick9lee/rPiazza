3d798908d9f23cb288eaaefaa0201bd2d2ea1c4e
AbelTekeste <47335877+AbelTekeste@users.noreply.github.com>
2023-03-31 16:20:49 -0600

    Added Comments to NGINX/Keepalived files

--- nginx/AutomateNGINX.py
+++ nginx/AutomateNGINX.py
@@ -1,28 +1,34 @@
 import subprocess
 from time import sleep
 
-subprocess.run("sudo systemctl start nginx", shell=True)
-subprocess.run("sudo systemctl start keepalived", shell=True)
+"""
+A simple implementation of Proxy Automation that runs when ever the EC2 instance is started.
+Will look at Keepalive and NGINX status and will restart if one is inactive or error is seen. 
+"""
+
+subprocess.run("sudo systemctl start nginx", shell=True) # Start nginx
+subprocess.run("sudo systemctl start keepalived", shell=True) # Start keepalived
 
 def restart_services():
+    """
+    Function that restarts the service and returns any errors seen.
+    """
     try:
-        sleep(1)
         print("Restarting Keepalived & NGINX")
-        subprocess.run("sudo systemctl start nginx", shell=True)
-        subprocess.run("sudo systemctl start keepalived", shell=True)
+        subprocess.run("sudo systemctl start nginx", shell=True) # restart nginx
+        subprocess.run("sudo systemctl start keepalived", shell=True) # restart keepalived
         print("Restarted")
-        sleep(1)
     except Exception as e:
         print(e) 
 
-while(1):
-    try:
+while(1): # Constantly check
+    try: # Check for active status is NGINX and Keepalived
         NGINX_State = subprocess.check_output("sudo systemctl is-active nginx", shell=True).decode("utf-8").strip()
         KA_State = subprocess.check_output("sudo systemctl is-active keepalived", shell=True).decode("utf-8").strip()
-    except:
+    except: # If error, then restart services
         print("Error occured in checking active status")
         restart_services()
-    else:
+    else: # If inactive, then restart services
         if KA_State == "inactive" or NGINX_State == "inactive":
             print("A service is inactive")
             restart_services()
