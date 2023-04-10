cf7b79701154d3f1682b662acf82786d22837cc4
Ahmed's Mackbook <ahmedsay01@gmail.com>
2023-04-10 12:50:28 -0600

    seperate app.jsx into components

---
+++ frontend/src/Timer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Timer.css";
+
+function Timer() {
+	return (
+		<div className="timeoutMessage">
+			Please Note that there is a 15 second cooldown between each tile draw.
+		</div>
+	);
+}
+
+export default Timer;
